93f323323bd2b9eac4737cfdafc32251d48b917e
Maxime Hu <maximeh.devweb@gmail.com>
2024-10-30 16:41:19 +0100

    render dynamic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 
 function App() {
-  return (
+  const age = 18;
+  let content = '';
+
+  if (age >= 18) {
+    content = 'Je suis majeur';
+  } else {
+    content = 'Je suis mineur';
+  }
+
+  return <div className="flex justify-center items-center">
     <h1 className="text-emerald-700">
-      Hello World, styled by TailwindCSS this title should be green
+      {content}
     </h1>
-  );
+  </div>
 }
 
 export default App;
