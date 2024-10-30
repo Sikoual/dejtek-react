c4152160e92bf4fdf2014cb9934b68e0d697bc57
Maxime Hu <maximeh.devweb@gmail.com>
2024-10-30 14:40:31 +0100

    add tailwind prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return <h1 className='text-emerald-700'>Hello World, styled by TailwindCSS this title should be green</h1>
+  return (
+    <h1 className="text-emerald-700">
+      Hello World, styled by TailwindCSS this title should be green
+    </h1>
+  );
 }
 
-export default App
+export default App;
